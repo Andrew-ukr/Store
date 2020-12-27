5d41d9aec8971144311f3138c7ff9f1e5f9163c1
Andrii <pashulyaand@gmail.com>
2020-12-27 14:51:42 +0200

    change img ater press color btn

--- #src/js/_selectColor.js
+++ #src/js/_selectColor.js
@@ -1,13 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
   let colorItem = document.querySelectorAll(".goods__color-item");
+  let sliderImg = document.querySelectorAll('.swiper-slide__img');
 
   colorItem.forEach(item => {
     item.addEventListener('click', () => {
+      let previousColorClass;
+      let currentColorClass;
       colorItem.forEach(item => {
+        if (item.classList.contains('active')) {
+          previousColorClass = item.classList.item(1);
+        }
         item.classList.remove('active');
       });
       item.classList.add('active');
+      currentColorClass = item.classList.item(1);
+      sliderImg.forEach(item => {
+        let sliderImgPath = item.getAttribute('src').replace(previousColorClass, currentColorClass);
+        item.setAttribute('src', sliderImgPath);
+        console.log(item.previousElementSibling.getAttribute('srcset'));
+        let sliderImgPathWebp = item.previousElementSibling.getAttribute('srcset').replace(previousColorClass, currentColorClass);
+        item.previousElementSibling.setAttribute('srcset', sliderImgPathWebp);
+      });
     });
   });
-
 });
