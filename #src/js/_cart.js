910c67e2c987a41e3ff91feb9a0e2360a1a63b52
Andrii <pashulyaand@gmail.com>
2020-12-30 15:46:06 +0200

    added data to local storage

--- #src/js/_cart.js
+++ #src/js/_cart.js
@@ -1,17 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
   const cartData = document.querySelectorAll(`[data-cart="data"]`);
-  
-  let a = {};
   let goods = {};
+  
+  let cart = [];
+  if (localStorage.getItem('cartItems')) {
+    cart = JSON.parse(localStorage.getItem('cartItems'));
+  } 
+
   cartData.forEach(item => {
     item.addEventListener('click', (e) => {
       const addToCartBtn = item.querySelector(`[data-cart="add"]`);
       const goodsTitle = item.querySelector(`[data-cart="title"]`);
       const goodsPrice = item.querySelector(`[data-cart="price"]`);
       const goodsSize = item.querySelector(`[data-cart="size"]`);
-      const goodsNumber= item.querySelector(`[data-cart="number"]`);
-      const goodsColor= item.querySelector(`.active[data-cart="color"]`);
-      const goodsId= item.querySelector(`[data-cart]`);
+      const goodsNumber = item.querySelector(`[data-cart="number"]`);
+      const goodsColor = item.querySelector(`.active[data-cart="color"]`);
+      const goodsId = item.querySelector(`[data-cart]`);
 
       if (e.target && e.target.dataset.cart === 'add') {
         goods.id = goodsId.getAttribute("data-id");
@@ -20,12 +24,12 @@ window.addEventListener('DOMContentLoaded', () => {
         goods.size = goodsSize.value;
         goods.number = goodsNumber.innerText;
         goods.color = goodsColor.title;
-        // console.log(goods);
 
-        Object.assign(a, goods);
-        console.log(a);
-        let jsonGoods = JSON.stringify(goods);
-        localStorage.setItem("cartItems", jsonGoods);
+
+        cart.push(goods);
+        localStorage.setItem('cartItems', JSON.stringify(cart));
+        cart = JSON.parse(localStorage.getItem('cartItems'));
+        console.log(cart);
       }
     });
   });
